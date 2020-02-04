e3934376614c4b4f1031df9cf6b203af0a502f33
mustafahamdanah <mustafahamdan1994@gmail.com>
2020-02-04 22:03:52 +0200

    add

---
+++ js/js-style.js
@@ -0,0 +1,19 @@
+alert("welcome")
+
+var age = prompt("what is your age ?");
+console.log(age);
+var greeting; 
+ 
+if (age > 0 && age<8   ) {     
+    greeting = 'children clothes site';   
+} 
+else if (age >= 8 && age <=18) {     
+    greeting = 'ِYoung clothes site';   
+} else if (age > 18 && age <=100) {     
+    greeting = 'Adult clothes site';   
+} else {     
+    greeting = 'Something went wrong!';   
+} 
+
+
+document.getElementById('result').innerText = greeting;
