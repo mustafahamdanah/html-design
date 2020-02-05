4d52329ae0c95a26c201c85b154bf99a29a56da7
mustafahamdanah <mustafahamdan1994@gmail.com>
2020-02-06 00:58:56 +0200

    aa

--- js/js-style.js
+++ js/js-style.js
@@ -1,22 +1,45 @@
 alert("welcome")
+   
 
 function createstore () {
 
     var age = prompt("what is your age ?");
     console.log(age);
-    var greeting; 
+    var write = prompt("What is your favorite club?(ac milan,inter milan)")
+    console.log(write);
+    var picture;
+    console.log(picture);
+    
+    // var greeting; 
+    var number = prompt("How many pieces of clothes do you want to buy?")
+
+    
+     for (var i=0;i<number;i++) {
+        if (write === "ac milan") {
+            picture = picture + '<img src="photos/theo.jpg">';
+          }
+          else if (write === "inter milan") {
+              picture = picture + '<img src="photos/inter.jpg" alt="ggg">';
+          }
+          else {
+              picture = "try again please!"
+          }
+     }
+     while (write !== 'ac milan' && write !== 'inter milan'){
+        write = prompt("What is your favorite club?(ac milan,inter milan)")
+     }
      
-    if (age > 0 && age<8   ) {     
-        greeting = 'children clothes site';   
-    } 
-    else if (age >= 8 && age <=18) {     
-        greeting = 'ِYoung clothes site';   
-    } else if (age > 18 && age <=100) {     
-        greeting = 'Adult clothes site';   
-    } else {     
-        greeting = 'Something went wrong!';   
-    } 
-    return greeting;
+    // if (age > 0 && age<8   ) {     
+    //     greeting = 'children clothes site';   
+    // } 
+    // else if (age >= 8 && age <=18) {     
+    //     greeting = 'ِYoung clothes site';   
+    // } else if (age > 18 && age <=100) {     
+    //     greeting = 'Adult clothes site';   
+    // } else {     
+    //     greeting = 'Something went wrong!';   
+    // } 
+    return picture;
 }
 
 
