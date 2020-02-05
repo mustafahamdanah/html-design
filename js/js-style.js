1d931f2db6db2957fabcf4fe3efbd8b3e6a1875e
mustafahamdanah <mustafahamdan1994@gmail.com>
2020-02-05 13:00:55 +0200

    add

--- js/js-style.js
+++ js/js-style.js
@@ -1,19 +1,24 @@
 alert("welcome")
 
-var age = prompt("what is your age ?");
-console.log(age);
-var greeting; 
- 
-if (age > 0 && age<8   ) {     
-    greeting = 'children clothes site';   
-} 
-else if (age >= 8 && age <=18) {     
-    greeting = 'ِYoung clothes site';   
-} else if (age > 18 && age <=100) {     
-    greeting = 'Adult clothes site';   
-} else {     
-    greeting = 'Something went wrong!';   
-} 
+function createstore () {
+
+    var age = prompt("what is your age ?");
+    console.log(age);
+    var greeting; 
+     
+    if (age > 0 && age<8   ) {     
+        greeting = 'children clothes site';   
+    } 
+    else if (age >= 8 && age <=18) {     
+        greeting = 'ِYoung clothes site';   
+    } else if (age > 18 && age <=100) {     
+        greeting = 'Adult clothes site';   
+    } else {     
+        greeting = 'Something went wrong!';   
+    } 
+    return greeting;
+}
 
 
-document.getElementById('result').innerText = greeting;
+
+document.getElementById('result').innerText = createstore();
